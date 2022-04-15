0f5672cb663a8d31237ebcbb8b6c8471d893f322
Chris Wright <chris@dxw.com>
2022-04-15 15:01:46 +0100

    Allow conditional WAF
    * Allows the WAF to conditionally launched

--- locals.tf
+++ locals.tf
@@ -4,4 +4,5 @@ locals {
   project_name                   = "${replace(local.site_url, ".", "-")}-${local.project_random_id}"
   cloudfront_tls_certificate_arn = var.cloudfront_tls_certificate_arn
   cloudfront_enable_ipv6         = var.cloudfront_enable_ipv6
+  cloudfront_enable_waf          = var.cloudfront_enable_waf
 }
