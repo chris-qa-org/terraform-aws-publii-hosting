1d1bed8895147e1268c18d67ff256edb37cb1c25
Chris Wright <chris@dxw.com>
2022-04-17 12:37:04 +0100

    Support creation of CloudFront TLS certificates and Route53 records

---
+++ data.tf
@@ -0,0 +1,5 @@
+data "aws_route53_zone" "default" {
+  count = local.route53_hosted_zone_options.id != "" ? 1 : 0
+
+  zone_id = local.route53_hosted_zone_options.id
+}
--- locals.tf
+++ locals.tf
@@ -7,4 +7,5 @@ locals {
   cloudfront_enable_waf                  = var.cloudfront_enable_waf
   cloudfront_enable_apex_to_www_redirect = var.cloudfront_enable_apex_to_www_redirect
   enable_publii_pretty_urls              = var.enable_publii_pretty_urls
+  route53_hosted_zone_options            = var.route53_hosted_zone_options
 }
