7366e9214ae5c5eaaadf199ccd1c688f252af518
Chris Wright <chris@dxw.com>
2022-05-28 11:53:41 +0100

    Add Readme key features and usage examples

---
+++ examples/full-launch-with-existing-route53-zone/README.md
@@ -0,0 +1,40 @@
+# Full launch with existing Route53 Zone 
+
+ - [main.tf](./main.tf)
+
+```
+provider "aws" {
+  region = "us-east-1"
+  alias  = "useast1"
+  default_tags {
+    tags = {
+      Project = "my-project"
+    }
+  }
+}
+
+resource "aws_route53_zone" "example" {
+  name = "example.com"
+}
+
+module "aws_publii_hosting" {
+  source  = "chris-qa-org/terraform-aws-publii-hosting/aws"
+  version = "v1.0.0"
+
+  providers = {
+    aws.useast1 = aws.useast1
+  }
+
+  site_url                               = "example.com"
+  s3_bucket_acl                          = "private"
+  cloudfront_enable_ipv6                 = true
+  cloudfront_enable_waf                  = true // Note: This will cost at least $5.00/month - https://aws.amazon.com/waf/pricing/ (default: false)
+  cloudfront_enable_apex_to_www_redirect = true
+  enable_publii_pretty_urls              = true
+  route53_hosted_zone_options = {
+    id                                        = aws_route53_zone.example.id
+    create_certificate_dns_validation_records = true
+    create_site_url_dns_records               = true
+  }
+}
+```
---
+++ examples/use-existing-certificate-and-create-own-route53-records/main.tf
@@ -0,0 +1,54 @@
+provider "aws" {
+  region = "us-east-1"
+  alias  = "useast1"
+  default_tags {
+    tags = {
+      Project = "my-project"
+    }
+  }
+}
+
+module "aws_publii_hosting" {
+  source  = "chris-qa-org/terraform-aws-publii-hosting/aws"
+  version = "v1.0.0"
+
+  providers = {
+    aws.useast1 = aws.useast1
+  }
+
+  site_url                               = "example.com"
+  s3_bucket_acl                          = "private"
+  cloudfront_tls_certificate_arn         = "arn:aws:acm:us-east-1:123456789012:certificate/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+  cloudfront_enable_ipv6                 = true
+  cloudfront_enable_waf                  = true // Note: This will cost at least $5.00/month - https://aws.amazon.com/waf/pricing/ (default: false)
+  cloudfront_enable_apex_to_www_redirect = true
+  enable_publii_pretty_urls              = true
+}
+
+resource "aws_route53_zone" "example" {
+  name = "example.com"
+}
+
+resource "aws_route53_record" "frontend" {
+  zone_id = aws_route53_zone.example.zone_id
+  name    = "www.example.com"
+  type    = "A"
+
+  alias {
+    name                   = module.aws_publii_hosting.aws_cloudfront_distribution_frontend.domain_name
+    zone_id                = module.aws_publii_hosting.aws_cloudfront_distribution_frontend.hosted_zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "apex_redirect" {
+  zone_id = aws_route53_zone.example.zone_id
+  name    = "example.com"
+  type    = "A"
+
+  alias {
+    name                   = module.aws_publii_hosting.aws_cloudfront_distribution_frontend_www_redirect.domain_name
+    zone_id                = module.aws_publii_hosting.aws_cloudfront_distribution_frontend_www_redirect.hosted_zone_id
+    evaluate_target_health = true
+  }
+}
