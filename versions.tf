7d200382b9b812ae0df3eb7623c778ea84223405
Stretch <chris@dxw.com>
2024-04-23 23:51:07 +0100

    Bump Terraform versions
    * Terraform v1.8.1
    * hashicorp/aws 5.46.0

--- versions.tf
+++ versions.tf
@@ -1,8 +1,8 @@
 terraform {
-  required_version = ">= 1.2.1"
+  required_version = ">= 1.8.1"
   required_providers {
     aws = {
-      version               = "~> 4.4"
+      version               = ">= 5.46.0"
       configuration_aliases = [aws.useast1]
     }
   }
